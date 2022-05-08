fbd5d2443cf1090c1f1b727597fa97379ee14ac8
ravi buchupalli <ravibuchupalli@ravis-MacBook-Pro.local>
2022-05-08 10:55:31 -0500

    home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Container>
+        <Row>
+
+          <Col>
+            <Link to="/">
+              <Card bg='light' className='card' style={{ width: '18rem' }}>
+                <Card.Body>
+                  <Card.Title>Employee</Card.Title>
+                  <Card.Text>
+                    Get employee details in the company.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Link>
+          </Col>
+
+          <Col>
+            <Card bg='light' className='card' style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title>Customer</Card.Title>
+                <Card.Text>
+                Get customer details of the company.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          
+          </Col>
+          <Col>
+            <Card bg='light' className='card' style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title> Prospect </Card.Title>
+                <Card.Text>
+                  get details of prospective customers.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          
+          </Col>
+        </Row>
+      </Container>      
+
+      
     </div>
   );
 }
